27622b9edcc4ff29e3848a33b3786cb8fbe5baad
Danish Riaz <danishriaz0609@outlook.com>
2020-06-16 20:39:40 +0500

    post model attached

--- 02_The_Paractical_Application_Server/controllers/feed.js
+++ 02_The_Paractical_Application_Server/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -30,17 +32,26 @@ exports.createPost = (req, res, next) => {
 
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: 'Danish Riaz'
-      },
-      createdAt: new Date()
+
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/book.jpeg',
+    creator: {
+      name: 'Danish Riaz'
     }
   });
+
+  post.save()
+    .then(record => {
+      console.log(record);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: record
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
 };
